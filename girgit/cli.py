c5ce6c9797b40cba24757d02c943f0ce80f216d0
Gohil-Rudra <rudra202021@gmail.com>
2026-04-26 20:21:50 +0530

    When you saved your project in the end !

---
+++ girgit/cli.py
@@ -0,0 +1,90 @@
+import argparse
+import os
+import sys
+import textwrap
+
+from . import data
+from . import base
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser("To execute the git commands")
+
+    subparser = parser.add_subparsers(dest="command")
+    subparser.required = True
+
+    # init
+    init_parser = subparser.add_parser("init",help="To initialize a repository",description="Git creates a hidden .git/ directory inside your current folder. That directory is the entire brain of Git for your project.")
+    init_parser.set_defaults(func=init)
+
+    # hash-object
+    hash_parser = subparser.add_parser("hash-object",help="To hash the file" , description="It will read the file and store it in object database and apply SHA1 from hashlib giving the hex-digest as output")
+    hash_parser.add_argument("file")
+    hash_parser.set_defaults(func=hash_object)
+
+    # cat-file
+
+    cat_file_parser = subparser.add_parser("cat-file",help="To view stored object",description="It will take input oid to retrieve the file stored at objects/oid")
+    cat_file_parser.add_argument("oid")
+    cat_file_parser.set_defaults(func=cat_file)
+
+    # write-tree
+
+    write_tree_parser = subparser.add_parser("write-tree",help="To hash directory",description="It will take current working directory and store it in /objects")
+    write_tree_parser.set_defaults(func=write_tree)
+
+    # read-tree
+    read_tree_parser = subparser.add_parser("read-tree",help="To extract the directory in current directory",description="It will take oid and recursively collect files from the trees and  ")
+    read_tree_parser.add_argument("tree")
+    read_tree_parser.set_defaults(func=read_tree)
+
+    # commit
+    commit_parser = subparser.add_parser("commit",help="To Save the changes !",description="It will create a commit object that stores meta data like author,time & date along with type and oid of data")
+    commit_parser.set_defaults(func=commit)
+    commit_parser.add_argument('--message','-m',required=True)
+
+    # log
+
+    log_parser = subparser.add_parser("log",help="To view all commits",description="it iteratively sees the parent oid of an oid and prints each commit")
+    log_parser.set_defaults(func=log)
+
+    return parser.parse_args()
+
+
+
+def init(args):
+    data.init()
+    print(f'Initialized Empty girgit repo at {os.path.join(os.getcwd(),data.GIT_DIR)}')
+
+def hash_object(args):
+    with open(args.file,'rb') as out:
+        print(data.hash_object(out.read()))
+
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.oid,expected=None))
+
+def write_tree(args):
+    print(base.write_tree())
+
+def read_tree(args):
+    base.read_tree(args.tree)
+
+def commit(args):
+    print(base.commit(args.message))
+
+def log(args):
+    oid = data.get_HEAD()
+    while oid:
+        commit = base.get_commit(oid)
+        print(f'commit : {oid}\n')
+        print(textwrap.indent(commit.message,'      '))
+        print('')
+
+        oid = commit.parent
+
+def main():
+    args = parse_args()
+    args.func(args)
+    
---
+++ girgit/data.py
@@ -0,0 +1,38 @@
+import os
+import hashlib
+
+GIT_DIR = '.girgit'
+
+# It deals with creation of .girgit repo
+def init():
+    os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects') # Creating the objects repo
+
+# The function of hash_object is to create the file and hash it then output the hexdigest
+def hash_object(data,type_='blob'): # We need to find OID and store the file inside objects/oid
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest()
+    path = f'{GIT_DIR}/objects/{oid}'
+    with open(path,'wb') as inp:
+        inp.write(obj)
+    return oid
+
+def get_object(oid,expected='blob'):
+    with open(f'{GIT_DIR}/objects/{oid}','rb') as out:
+        obj = out.read()
+    type_,null_,content = obj.partition(b'\x00')
+    type_ = type_.decode()
+    if expected is not None :
+        assert type_ == expected , f'Wanted {expected} type , got {type_}'
+    return content # This is bytes data
+
+def set_HEAD(oid):
+    with open (f'{GIT_DIR}/HEAD','w') as inp :
+        inp.write(oid)
+
+def get_HEAD():
+    try:
+        with open(f'{GIT_DIR}/HEAD') as out:
+            return out.read()
+    except (FileNotFoundError,IsADirectoryError):
+        return None
